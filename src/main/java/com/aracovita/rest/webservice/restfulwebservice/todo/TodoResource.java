7eb85d339d581e8d955b8c74bf60a826cab94581
Andreea Racovita <andreearacovitagit@gmail.com>
2023-12-05 09:17:59 +0200

    Add addTodo logic

--- src/main/java/com/aracovita/rest/webservice/restfulwebservice/todo/TodoResource.java
+++ src/main/java/com/aracovita/rest/webservice/restfulwebservice/todo/TodoResource.java
@@ -1,5 +1,6 @@
 package com.aracovita.rest.webservice.restfulwebservice.todo;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -47,5 +50,17 @@ public class TodoResource {
 	) {
 		Todo todoUpdated = todoService.save(todo);
 		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+	}
+	
+	@PostMapping("/users/{username}/todos")
+	public ResponseEntity<Void> addTodo(
+			@PathVariable String username,
+			@RequestBody Todo todo
+	) {
+		Todo todoCreated = todoService.save(todo);
+		
+		// Location: get current resource url
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(todoCreated.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
