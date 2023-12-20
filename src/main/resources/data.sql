190bf456a31ad3c68e8990b6d8815b5f0ef52b24
Andreea Racovita <andreearacovitagit@gmail.com>
2023-12-20 16:19:47 +0200

    Add jpa support

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values (101, 'aracovita', 'Go scuba diving', CURRENT_DATE, false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (102, 'aracovita', 'Hike the Alps', CURRENT_DATE, false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (103, 'aracovita', 'Run a marathon', CURRENT_DATE, false);
